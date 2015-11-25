0d63f40677c3e13e9c033bbfee4d03c977fc1028
Frank Da Silva <frank.dasilva@gmail.com>
2015-11-25 16:59:07 +0000

    Scenario 1 passed

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,14 @@
+Given /^the following movies exist:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  # pending # express the regexp above with the code you wish you had
+  table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Movie.create(movie)
+  end
+end
+
+Then /^the director of "(.*?)" should be "(.*?)"$/ do |arg1, arg2|
+  #pending # express the regexp above with the code you wish you had
+  expect(Movie.find_by_title(arg1).director).to be == arg2
+end
