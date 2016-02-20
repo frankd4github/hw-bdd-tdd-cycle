4182e6a99e5a91d94bc7b51d02d65d35bb0105ea
Frank Da Silva <frank.dasilva@gmail.com>
2016-02-20 03:51:42 +0000

    before removing from c9

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -17,3 +17,49 @@ Given /^I am on the details page for "(.*?)"$/ do |title|
   #pending # express the regexp above with the code you wish you had
   visit movie_path(Movie.find_by_title title)
 end
+
+And /I am on the home page/ do
+  visit movies_path
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  #page.body.should =~ /#{e1}.*#{e2}/m
+  #expect(page.body).to match(/#{e1}.*#{e2}/m)
+  td_elements = page.all("tbody td")
+  i1 = td_elements.find_index {|td| td.text == e1}
+  i2 = td_elements.find_index {|td| td.text == e2}
+  expect(i1).to be <= i2
+  #fail "Unimplemented"
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/,\s*/).each do |rating|
+    case uncheck
+    when ''
+      # check "ratings[#{rating[1..-2]}]"
+      step %Q{I check "ratings[#{rating[1..-2]}]"}
+    when 'un'
+      # uncheck "ratings[#{rating[1..-2]}]"
+      step %Q{I uncheck "ratings[#{rating[1..-2]}]"}
+    end
+  end
+  #fail "Unimplemented"
+end
+
+Then /I should see all the movies/ do
+  # Make sure that all the movies in the app are visible in the table
+  expect(page.all('tbody tr').length).to be(Movie.all.length)
+  #fail "Unimplemented"
+end
